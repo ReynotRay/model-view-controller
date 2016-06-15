676c819edc706e524eff52f8bb7a6b505a5e11db
Rey Garcia <reynaldogarcia@Reynaldos-iMac.attlocal.net>
2016-06-15 11:45:31 -0500

    edit:

--- js/app.js
+++ js/app.js
@@ -1,45 +1,55 @@
-var mvcExample = {};
 
-mvcExample.Model = function(){
-	this.text = "";
-	this.onChange = null;
+
+//Model
+//create cronstructor for model
+var Model = function() {
+// create two attributes this.text and this.onChange    
+    this.text = "";
+//this can hold a CALLBACK, it will notify other parts of the app when the model has changed.    
+    this.onChange = null;
 };
+// we create a method which can change the stored text and calls the change
+Model.prototype.setText = function(value) {
+    //string to uppercase method
+    this.text = value.toUpperCase();
+    //will change the text
+    if (this.onChange) {
+        this.onChange(this.text);
+    }
+  };
+//view
+ //this contructor valled view will take on two parameters elementSlector and initialValue
+var View = function(elementSelector, initialValue) {
+    //use jquery to select the input
+    this.element = $(elementSelector);
+   //set value method
+    this.setValue(initialValue || '');
 
-mvcExample.Model.prototype.setText = function (value) {
-	this.text = value.toUpperCase();
-	if (this.onChange) {
-		this.onChange(this.text);
-	}
+    this.onChange = null;
+ //sets the <input> elemnts value using the setValue method.
+    this.element.on('input', this.onInput.bind(this));
 };
-
-mvcExample.View = function (elementId, intialValue){
-	this.element = document. getElementById(elementId);
-	this.setValue(intialValue || '');
-	this.onInput = null;
-
-	this.element.addEventListener('input', this._onInput.bind(this));
-
+ //when user types something the method onInput will be callback to onChange
+View.prototype.onInput = function() {
+    var value = this.element.val();
+    if (this.onChange) {
+        this.onChange(value);
+    }
 };
-
-mvcExample.View.prototype._onInput = function(event) {
-	var value = event.target.value;
-	if (this.onIn[put]){
-		this.onInput(value);
-
-	}
+//get the value in text formate
+View.prototype.setValue = function(text) {
+    this.element.val(text);
 };
-mvcExample.View.prototype.setValue = function(text){
-	this.element.value = text;
-};
-
-mvcExample.Controller = function(model, view) {
-    view.onInput = model.setText.bind(model);
+//controller
+//user enters something in the view, the models text is then changed, and then whe the models text
+//is changed the view has its value updated. 
+var Controller = function(model, view) {
+    view.onChange = model.setText.bind(model);
     model.onChange = view.setValue.bind(view);
 };
-
-document.addEventListener('DOMContentLoaded', function(){
-	var model = new mvcExample.Model();
-	var view = new mvcExample.View('toUpperCase');
-	var controller = new mvcExample.Controller(model, view);
-
+//we construct three elements. 
+document.addEventListener('DOMContentLoaded', function() {
+    var model = new Model();
+    var view = new View('uppercase');
+    var controller = new Controller(model, view);
 });
